b823995bdb2840e30ff69b8a1da8974b73137860
Maciej <01187214@pw.edu.pl>
2025-11-18 11:01:34 +0100

    Added empty implementation file

---
+++ src/Config.cc
@@ -0,0 +1,9 @@
+#include "config.h"
+
+
+namespace Config {
+    void initializeRuntimeConstants(const unsigned int& lawn_width, const unsigned int& lawn_length, 
+        const unsigned int& blade_diameter, const unsigned int& mover_width, const unsigned int& mover_length) {
+        return;
+    }
+}
