4eaee01690a5b80d4986cdd0b3850fe221fa8aaf
Maciej <01187214@pw.edu.pl>
2025-11-18 21:15:07 +0100

    Capitalised main.cc and added std namespace

---
+++ src/Main.cc
@@ -0,0 +1,14 @@
+/* 
+    Author: Maciej Cieslik
+    
+    Main program file
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
