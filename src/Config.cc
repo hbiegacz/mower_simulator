9343f8923af7874f8100472ba65d662e58fd788b
Maciej <01187214@pw.edu.pl>
2025-11-18 21:15:39 +0100

    Impelemented runtime constants calculation

--- src/Config.cc
+++ src/Config.cc
@@ -1,9 +1,33 @@
-#include "config.h"
+/* 
+    Author: Maciej Cieslik
+    
+    Implements global constants computing at program start.
+*/
+#include <algorithm>
+#include "Constants.h"
+#include "Config.h"
 
+using namespace std;
 
 namespace Config {
-    void initializeRuntimeConstants(const unsigned int& lawn_width, const unsigned int& lawn_length, 
-        const unsigned int& blade_diameter, const unsigned int& mover_width, const unsigned int& mover_length) {
-        return;
+    void initializeRuntimeConstants(const unsigned int& lawn_width, const unsigned int& lawn_length) {
+
+        const unsigned int ABSOLUTE_MINIMAL_BLADE_DIAMETER = 10; // cm
+        const unsigned int MIN_LAWN_DIVISION_FACTOR = 1000;
+        MIN_BLADE_DIAMETER = max(ABSOLUTE_MINIMAL_BLADE_DIAMETER, 
+            max(lawn_width / MIN_LAWN_DIVISION_FACTOR, lawn_length / MIN_LAWN_DIVISION_FACTOR));
+
+        const unsigned int ABSOLUTE_MAXIMAL_BLADE_DIAMETER = 100; // cm
+        const unsigned int MAX_LAWN_DIVISION_FACTOR = 10;
+        MAX_BLADE_DIAMETER = min(ABSOLUTE_MAXIMAL_BLADE_DIAMETER, 
+            min(lawn_width / MAX_LAWN_DIVISION_FACTOR, lawn_length / MAX_LAWN_DIVISION_FACTOR));
+
+        const unsigned int MOVER_SIZE_MULTIPLICATON_FACTOR = 2;
+        MIN_MOVER_WIDTH = MIN_BLADE_DIAMETER;
+        MAX_MOVER_WIDTH = MOVER_SIZE_MULTIPLICATON_FACTOR * MAX_BLADE_DIAMETER;
+        MIN_MOVER_LENGTH = MIN_MOVER_WIDTH;
+        MAX_MOVER_LENGTH = MAX_MOVER_WIDTH;
+        
+        FIELD_WIDTH = min(lawn_width, lawn_length) / 1000;
     }
 }
