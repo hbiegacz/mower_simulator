e0c98a244b5583bbe5c1fb9e61b91d5c4f9941d5
Maciej <01187214@pw.edu.pl>
2025-11-21 14:35:50 +0100

    Added tests for isPointInLawn method

--- tests/LawnTests.cc
+++ tests/LawnTests.cc
@@ -7,7 +7,8 @@
 #include <gtest/gtest.h>
 #include "../include/Lawn.h"
 
-TEST(InitializeRuntimeConstantsTest, Getters) {
+
+TEST(Getters, Getters) {
     unsigned int lawn_width = 100;
     unsigned int lawn_length = 100;
     Lawn lawn = Lawn(lawn_width, lawn_length);
@@ -17,4 +18,68 @@ TEST(InitializeRuntimeConstantsTest, Getters) {
 
     EXPECT_EQ(width, lawn_width);
     EXPECT_EQ(length, lawn_length);
+}
+
+
+TEST(IsPointInLawn, isPointInLawnCorrect) {
+    unsigned int lawn_width = 100;
+    unsigned int lawn_length = 100;
+    Lawn lawn = Lawn(lawn_width, lawn_length);
+    double x = 0.5;
+    double y = 0.3;
+
+    bool result = lawn.isPointInLawn(x, y);
+
+    EXPECT_EQ(true, result);
+}
+
+TEST(IsPointInLawn, isPointInLawnCorrectMinimalValues) {
+    unsigned int lawn_width = 100;
+    unsigned int lawn_length = 100;
+    Lawn lawn = Lawn(lawn_width, lawn_length);
+    double x = 0.0;
+    double y = 0.0;
+
+    bool result = lawn.isPointInLawn(x, y);
+
+    EXPECT_EQ(true, result);
+}
+
+
+TEST(IsPointInLawn, isPointInLawnCorrectMaximalValues) {
+    unsigned int lawn_width = 100000;
+    unsigned int lawn_length = 100000;
+    Lawn lawn = Lawn(lawn_width, lawn_length);
+    double x = 100000.0;
+    double y = 100000.0;
+
+    bool result = lawn.isPointInLawn(x, y);
+
+    EXPECT_EQ(true, result);
+}
+
+
+TEST(IsPointInLawn, isPointInLawnIncorrectSquareLawn) {
+    unsigned int lawn_width = 60000;
+    unsigned int lawn_length = 60000;
+    Lawn lawn = Lawn(lawn_width, lawn_length);
+    double x = 60000.1;
+    double y = 50000.1;
+
+    bool result = lawn.isPointInLawn(x, y);
+
+    EXPECT_EQ(false, result);
+}
+
+
+TEST(IsPointInLawn, isPointInLawnIncorrectRectangularLawn) {
+    unsigned int lawn_width = 60000;
+    unsigned int lawn_length = 6000;
+    Lawn lawn = Lawn(lawn_width, lawn_length);
+    double x = 50000.1;
+    double y = 6000.1;
+
+    bool result = lawn.isPointInLawn(x, y);
+
+    EXPECT_EQ(false, result);
 }
