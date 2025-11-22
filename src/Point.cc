c235af5a8f8111386267ca9c578cb0bbc75fb161
hbiegacz <biegacz.hanna@gmail.com>
2025-11-22 13:46:36 +0100

    implemented basic functionalities (getters, setters constructors)

---
+++ src/Point.cc
@@ -0,0 +1,41 @@
+/* 
+    Author: Hanna Biegacz
+    
+    Implementation of Point struct for lawn mower simulator. 
+    Point represents a marker on the lawn that the mower should reach.
+*/
+
+#include "../include/Point.h"
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+unsigned int Point::idCounter_ = 0;
+
+
+Point::Point() 
+    : x_(0.0), y_(0.0), id_(idCounter_++) {}
+
+
+Point::Point(const double& x, const double& y)
+    : x_(x), y_(y), id_(idCounter_++) {}
+
+
+double Point::getX() const {
+    return x_;
+}
+
+double Point::getY() const {
+    return y_;
+}
+
+unsigned int Point::getId() const {
+    return id_;
+}
+
+void Point::setPosition(const double& x, const double& y) {
+    x_ = x;
+    y_ = y;
+}
