1434aa114419cad195e3ba1358baf6a803a9907e
Maciej <01187214@pw.edu.pl>
2025-12-06 22:25:31 +0100

    Added test for move method

--- tests/MoverTests.cc
+++ tests/MoverTests.cc
@@ -5,8 +5,9 @@
 */
 
 #include <gtest/gtest.h>
-#include "../include/Mover.h"
 #include "../include/Config.h"
+#include "../include/Mover.h"
+#include "../include/Exceptions.h"
 
 using namespace std;
 
@@ -58,4 +59,288 @@ TEST(Setters, setters) {
     EXPECT_EQ(angle, result_angle);
     EXPECT_NEAR(x, result_x, 1e-9);
     EXPECT_NEAR(y, result_y, 1e-9);
+}
+
+
+TEST(Move, moveInsideLawnDefaultSpawnPoint) {
+    unsigned short angle = 45;
+    double distance = 100;
+    double x = 70.711;
+    double y = 70.711;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0.0, 0.0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnDoubleDistanceSpawnPoint) {
+    unsigned short angle = 45;
+    double distance = 50.345;
+    double x = 35.599;
+    double y = 35.599;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0.0, 0.0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnHardAngleDefaultSpawnPoint) {
+    unsigned short angle = 37;
+    double distance = 100;
+    double x = 60.1815;
+    double y = 79.8635;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0.0, 0.0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnHardAngleDoubleDistanceDefaultSpawnPoint) {
+    unsigned short angle = 37;
+    double distance = 50.345;
+    double x = 30.2983;
+    double y = 40.2073;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0.0, 0.0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnHardAngleDoubleDistanceCustomSpawnPoint) {
+    unsigned short angle = 37;
+    double distance = 50.345;
+    double x = 60.596;
+    double y = 80.414;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 30.298, 40.207, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnBorderXDefaultSpawnPoint) {
+    unsigned short angle = 90;
+    double distance = 100;
+    double x = 100;
+    double y = 0;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveInsideLawnBorderYDefaultSpawnPoint) {
+    unsigned short angle = 0;
+    double distance = 100;
+    double x = 0;
+    double y = 100;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+    mover.move(distance, lawn_width, lawn_length);
+
+    unsigned int result_angle = mover.getAngle();
+    double result_x = mover.getX();
+    double result_y = mover.getY();
+
+    EXPECT_EQ(angle, result_angle);
+    EXPECT_NEAR(x, result_x, 1e-3);
+    EXPECT_NEAR(y, result_y, 1e-3);
+}
+
+
+TEST(Move, moveOutsideLawnBorderYDefaultSpawnPointMinimalDeltaX) {
+    unsigned short angle = 0;
+    double distance = 105.001;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
+}
+
+
+TEST(Move, moveOutsideLawnBorderYDefaultSpawnPointMinimalDeltaY) {
+    unsigned short angle = 0;
+    double distance = 120.0001;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
+}
+
+
+TEST(Move, moveOutsideLawnBorderDefaultSpawnPoint) {
+    unsigned short angle = 45;
+    double distance = 200;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
+}
+
+
+TEST(Move, moveOutsideLawnBorderDefaultSpawnPointNegativeX) {
+    unsigned short angle = 270;
+    double distance = 5.001;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
+}
+
+
+TEST(Move, moveOutsideLawnBorderDefaultSpawnPointNegativeY) {
+    unsigned short angle = 180;
+    double distance = 5.001;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
+}
+
+
+TEST(Move, moveOutsideLawnBorderDefaultSpawnPointNegativeXY) {
+    unsigned short angle = 225;
+    double distance = 10;
+    unsigned int width = 10;
+    unsigned int length = 10;
+    unsigned int blade_diameter = 90;
+    unsigned int speed = 105;
+    unsigned int lawn_width = 120;
+    unsigned int lawn_length = 100;
+    Config::initializeRuntimeConstants(lawn_width, lawn_length);
+    Config::initializeMoverConstants(width, length, 0, 0, angle);
+    Mover mover = Mover(width, length, blade_diameter, speed);
+
+    EXPECT_THROW({mover.move(distance, lawn_width, lawn_length);}, MoveOutsideLawnError);
 }
