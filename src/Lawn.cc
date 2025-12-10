be1511014d937e741437c7f03cc12c80df39c177
Maciej <01187214@pw.edu.pl>
2025-12-10 19:05:37 +0100

    Impelemenetd cutGradd mehtod and helper ones

--- src/Lawn.cc
+++ src/Lawn.cc
@@ -92,5 +92,70 @@ double Lawn::calculateShavedArea() {
 
 
 void Lawn::cutGrass(const pair<double, double>& blade_middle, const unsigned int& blade_diameter) {
+    pair<unsigned int, unsigned int> first_indexes = calculateFieldIndexes(blade_middle.first - blade_diameter / 2,
+        blade_middle.second - blade_diameter / 2);
+    double beginning_x = static_cast<double>(first_indexes.first) * Config::FIELD_WIDTH;
+    double beginning_y = static_cast<double>(first_indexes.second) * Config::FIELD_WIDTH;
+    double ending_x = max(beginning_x + blade_diameter, static_cast<double>(width_));
+    double ending_y = max(beginning_y + blade_diameter, static_cast<double>(length_));
+    double current_y = beginning_y;
+    double current_x = beginning_x;
 
+    while (current_y < ending_y) {
+        while (current_x < ending_x) {
+            if (isFieldInMowingArea(current_x, current_y, blade_middle, blade_diameter)) {
+                pair<unsigned int, unsigned int> indexes = calculateFieldIndexes(current_x, current_y);
+                cutGrassOnField(indexes);
+            }
+            current_x = current_x + Config::FIELD_WIDTH;
+        }
+        current_y = current_y + Config::FIELD_WIDTH;
+        current_x = beginning_x;
+    }
+}
+
+
+bool Lawn::isFieldInMowingArea(const double& x, const double& y, const std::pair<double, double>& blade_middle, 
+        const double& blade_diameter) {
+    unsigned int counter = countCornersInArea(x, y, blade_middle, blade_diameter);
+    
+    if (counter > 2) {
+        return true;
+    }
+    else if (counter == 2) {
+        return calculateDistanceBetweenPoints(x + Config::FIELD_WIDTH / 2, y + Config::FIELD_WIDTH / 2,
+            blade_middle) <= blade_diameter;
+    }
+    else {
+        return false;
+    }
+}
+
+
+double Lawn::calculateDistanceBetweenPoints(const double& x, const double& y, 
+    const std::pair<double, double>& destination_point) {
+    double dx = x - destination_point.first;
+    double dy = y - destination_point.second;
+
+    return std::sqrt(dx*dx + dy*dy);
+}
+
+
+unsigned int Lawn::countCornersInArea(const double& x, const double& y, const std::pair<double, double>& blade_middle, 
+        const double& blade_diameter) {
+    pair<double, double> points[4] = {
+        {x, y},
+        {x + Config::FIELD_WIDTH, y},
+        {x + Config::FIELD_WIDTH, y + Config::FIELD_WIDTH},
+        {x, y + Config::FIELD_WIDTH}
+    };
+
+    unsigned int counter = 0;
+    for (pair<double, double> point : points) {
+        if (calculateDistanceBetweenPoints(x, y, blade_middle) <= blade_diameter) {
+            counter ++;
+        }
+    }
+
+    return counter;
 }
