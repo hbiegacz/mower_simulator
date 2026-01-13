3301e03fbd9c49b874d16e12f433e0430557018c
hbiegacz <biegacz.hanna@gmail.com>
2026-01-13 18:32:04 +0100

    removed outdated main logic

--- src/Main.cc
+++ src/Main.cc
@@ -8,39 +8,16 @@
 #include <QApplication>
 #include <iostream>
 #include "../include/Lawn.h"
-#include "simulation/SimulationEngine.h"
+#include "../include/Mover.h"
+#include "../include/Config.h"
+#include "../include/Logger.h"
+#include "../include/FileLogger.h"
+#include "../include/StateSimulation.h"
+#include "Engine.h"
 #include "simulation/LawnSimulationView.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    constexpr unsigned int LAWN_WIDTH_CM = 500;
-    constexpr unsigned int LAWN_LENGTH_CM = 400;
-    constexpr double SIMULATION_SPEED_MULTIPLIER = 10000.0;
-    constexpr int DISPLAY_FPS = 30;
     
-    QApplication app(argc, argv);
-    
-    cout << "[Main] Creating lawn: " << LAWN_WIDTH_CM << "x" << LAWN_LENGTH_CM << " cm" << endl;
-    Lawn lawn(LAWN_WIDTH_CM, LAWN_LENGTH_CM);
-    
-    cout << "[Main] Initializing simulation engine" << endl;
-    SimulationEngine engine(&lawn);
-    engine.setSimulationSpeed(SIMULATION_SPEED_MULTIPLIER);
-    
-    cout << "[Main] Creating window" << endl;
-    LawnSimulationView simulation_view(&lawn, engine.getLawnMutex()); 
-    simulation_view.setWindowTitle("Lawn Mower Simulator");    
-    
-    simulation_view.show();
-    simulation_view.startSimulation(DISPLAY_FPS);    
-    engine.start();
-    
-    cout << "[Main] Starting event loop" << endl;
-    const int result = app.exec();
-    
-    cout << "[Main] Stopping simulation" << endl;
-    engine.stop();
-    
-    return result;
 }
